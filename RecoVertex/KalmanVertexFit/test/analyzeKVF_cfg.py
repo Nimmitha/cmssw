5e5d6a4bdaeb56553e8b3b5aaf5de1c8a5483a64
Nimmitha Karunarathna <nimmitha..karunarathna@cern.ch>
2023-06-16 03:08:18 +0200

    initial

--- RecoVertex/KalmanVertexFit/test/analyzeKVF_cfg.py
+++ RecoVertex/KalmanVertexFit/test/analyzeKVF_cfg.py
@@ -8,11 +8,13 @@ process.load("Configuration.StandardSequences.MagneticField_cff")
 
 process.load("Configuration.StandardSequences.Services_cff")
 
-process.load("Configuration.StandardSequences.GeometryDB_cff")
+# process.load("Configuration.StandardSequences.GeometryDB_cff")
+process.load("Configuration.StandardSequences.GeometryRecoDB_cff") # Jesse
 
-process.load("Configuration.StandardSequences.FakeConditions_cff")
+# process.load("Configuration.StandardSequences.FakeConditions_cff")
 
-process.load("TrackingTools.TransientTrack.TransientTrackBuilder_cfi")
+# process.load("TrackingTools.TransientTrack.TransientTrackBuilder_cfi")
+process.load('Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff') # Jesse
 
 process.load("SimTracker.TrackAssociatorProducers.trackAssociatorByChi2_cfi")
 
@@ -20,7 +22,9 @@ process.load("SimTracker.TrackAssociatorProducers.trackAssociatorByHits_cfi")
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-       '/store/relval/CMSSW_2_1_8/RelValTTbar/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/IDEAL_V9_v1/0002/04983078-9082-DD11-BB8C-0019DB2F3F9B.root')
+    #    '/store/relval/CMSSW_2_1_8/RelValTTbar/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/IDEAL_V9_v1/0002/04983078-9082-DD11-BB8C-0019DB2F3F9B.root'
+    'file:/eos/home-n/nkarunar/2023Analysis/datasets/2022C/7afde7ee-e709-46e9-ae45-7725c28af2bb.root'   
+       )
 )
 
 process.simpleVertexAnalysis = cms.EDAnalyzer("KVFTest",
