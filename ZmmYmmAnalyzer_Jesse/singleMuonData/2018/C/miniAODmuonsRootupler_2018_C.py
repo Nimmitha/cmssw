3411d2f35babdbed662a11d6dad3cf122f12dd5b
Nimmitha Karunarathna <nimmithak@gmail.com>
2024-06-06 04:24:52 -0500

    config files to run 2018 data

---
+++ ZmmYmmAnalyzer_Jesse/singleMuonData/2018/C/miniAODmuonsRootupler_2018_C.py
@@ -0,0 +1,54 @@
+import FWCore.ParameterSet.Config as cms
+process = cms.Process("Rootuple")
+
+process.load("TrackingTools.TransientTrack.TransientTrackBuilder_cfi")
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
+from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, '106X_dataRun2_v32', '')
+
+from RecoEgamma.EgammaTools.EgammaPostRecoTools import setupEgammaPostRecoSeq
+setupEgammaPostRecoSeq(process,
+                       #runVID=True, #if you want the Fall17V2 IDs, set this to True or remove (default is True)
+                       runEnergyCorrections=True,
+                       runVID=True,
+                       #eleIDModules=['RecoEgamma.ElectronIdentification.Identification.mvaElectronID_Summer16_ID_ISO_cff','RecoEgamma.ElectronIdentification.Identification.heepElectronID_HEEPV70_cff'],
+                       era='2018-UL')                       
+#                       )  #era is new to select between 2016 / 2017,  it defaults to 2017
+
+process.MessageLogger.cerr.FwkReport.reportEvery = 200
+process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
+process.options.allowUnscheduled = cms.untracked.bool(True)
+#process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))#,SkipEvent = cms.untracked.vstring('ProductNotFound'))
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring('file:test.root')
+)
+
+process.rootuple = cms.EDAnalyzer('miniAODmuons',
+                          dimuons = cms.InputTag("slimmedMuons"),
+                          dielectron = cms.InputTag("slimmedElectrons"),
+                          Trak = cms.InputTag("packedPFCandidates"),
+                          primaryVertices = cms.InputTag("offlineSlimmedPrimaryVertices"),
+                          bits = cms.InputTag("TriggerResults::HLT"),
+                          objects = cms.InputTag("slimmedPatTrigger"),
+                          pruned = cms.InputTag("prunedGenParticles"),
+                          MuonTrigger = cms.string("HLT_IsoMu24_v"),
+                          # ElectronTrigger = cms.string("HLT_Ele27_WPTight_Gsf_v"),
+                          # DataType = cms.string("2018C_UL"),  # Title of the output ROOT file
+                                  isMC = cms.bool(False),
+                          )
+
+process.TFileService = cms.Service("TFileService",
+  fileName = cms.string('SingleMuon_Run2018C_UL_v3_Data.root'),
+)
+
+process.p = cms.Path(process.egammaPostRecoSeq+process.rootuple)
+
