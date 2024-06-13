5bb22fe1867d5ad951245ccc6fa6591e37fc04f9
Nimmitha Karunarathna <nimmithak@gmail.com>
2024-06-13 04:12:56 -0500

    config files for run2 datasets

---
+++ ZmmYeeAnalyzer/singleMuonData/2017/H/crabConfig_TTree.py
@@ -0,0 +1,29 @@
+from CRABClient.UserUtilities import config
+config = config()
+
+# user specific generic parameters
+config.General.requestName = 'TTree_13TeV_fourmuon_2017H'   # Used as the task/Project directory name
+config.General.transferOutputs = True                               # Transfer output files to the storage site
+config.General.transferLogs = False
+
+# job type and related configurables
+config.JobType.pluginName = 'Analysis'                              # Specify: analysis or MC generation
+config.JobType.psetName = 'miniAODmuonsRootupler_2017_H.py'           # parameter-set config file
+config.JobType.allowUndistributedCMSSW = True                       # Allow CMSSW release possibly not available at sites
+config.JobType.outputFiles = ['SingleMuon_Run2017H_UL_LowPU_v1_Data.root']   # List of output files that needs to be collected
+# config.JobType.maxMemoryMB = 1000
+
+# data to be analyzed
+config.Data.inputDBS = 'global'
+config.Data.inputDataset = '/SingleMuon/Run2017H-09Aug2019_UL2017_LowPU-v1/MINIAOD'               # Name of the dataset
+#config.Data.outputPrimaryDataset = 'DoubleJPsiToMuMu_RAWSIM_SPS_LO_may2016_largetest_FNAL' # Used when running private input files or MC generation
+config.Data.lumiMask = 'Cert_294927-306462_13TeV_UL2017_Collisions17_MuonJSON.txt' # Lumi-section filter
+config.Data.splitting = 'LumiBased'                                                         # Split the task based on 
+config.Data.unitsPerJob = 50                                                                # Number of splitted units per job
+#NJOBS = 500  # This is not a configuration parameter, but an auxiliary variable that we use in the next line.
+config.Data.totalUnits = -1                                                                 # Number of untis to analyze 
+config.Data.outLFNDirBase = '/store/user/nkarunar/'
+config.Data.publication = False                                                             # Whether to publish the EDM output files in DBS
+
+# Grid site parameters
+config.Site.storageSite = 'T3_US_FNALLPC'         # Place to copy the output files
---
+++ ZmmYeeAnalyzer/singleMuonData/2018/D/crabConfig_TTree_Zee.py
@@ -0,0 +1,29 @@
+from CRABClient.UserUtilities import config
+config = config()
+
+# user specific generic parameters
+config.General.requestName = 'TTree_13TeV_fourmuon_2018D_Zee'   # Used as the task/Project directory name
+config.General.transferOutputs = True                               # Transfer output files to the storage site
+config.General.transferLogs = False
+
+# job type and related configurables
+config.JobType.pluginName = 'Analysis'                              # Specify: analysis or MC generation
+config.JobType.psetName = 'miniAODmuonsRootupler_2018_D_Zee.py'           # parameter-set config file
+config.JobType.allowUndistributedCMSSW = True                       # Allow CMSSW release possibly not available at sites
+config.JobType.outputFiles = ['SingleMuon_Run2018D_UL_v8_Data_Zee.root']   # List of output files that needs to be collected
+# config.JobType.maxMemoryMB = 1000
+
+# data to be analyzed
+config.Data.inputDBS = 'global'
+config.Data.inputDataset = '/SingleMuon/Run2018D-12Nov2019_UL2018-v8/MINIAOD'               # Name of the dataset
+#config.Data.outputPrimaryDataset = 'DoubleJPsiToMuMu_RAWSIM_SPS_LO_may2016_largetest_FNAL' # Used when running private input files or MC generation
+config.Data.lumiMask = 'Cert_314472-325175_13TeV_Legacy2018_Collisions18_JSON_MuonPhys.txt' # Lumi-section filter
+config.Data.splitting = 'LumiBased'                                                         # Split the task based on 
+config.Data.unitsPerJob = 50                                                                # Number of splitted units per job
+#NJOBS = 500  # This is not a configuration parameter, but an auxiliary variable that we use in the next line.
+config.Data.totalUnits = -1                                                                 # Number of untis to analyze 
+config.Data.outLFNDirBase = '/store/user/nkarunar/'
+config.Data.publication = False                                                             # Whether to publish the EDM output files in DBS
+
+# Grid site parameters
+config.Site.storageSite = 'T3_US_FNALLPC'         # Place to copy the output files
