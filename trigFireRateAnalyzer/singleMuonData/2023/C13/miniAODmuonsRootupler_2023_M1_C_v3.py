87dac98822b3ac8b158b67de0e1c94ef37162934
Nimmitha Karunarathna <nimmithak@gmail.com>
2024-06-17 23:13:54 -0500

    data config files for 2022 and 2023

---
+++ trigFireRateAnalyzer/singleMuonData/2023/C13/miniAODmuonsRootupler_2023_M1_C_v3.py
@@ -0,0 +1,52 @@
+import FWCore.ParameterSet.Config as cms
+process = cms.Process("Rootuple")
+
+process.load("TrackingTools.TransientTrack.TransientTrackBuilder_cfi")
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+
+#process.load('RecoEgamma.ElectronIdentification.Identification.mvaElectronID_Summer16_ID_ISO_cff')
+# process.load("Configuration.Geometry.GeometryIdeal_cff")
+
+# process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
+# from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
+# process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_mc') # Tried: 106X_dataRun2_v32
+
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, '130X_dataRun3_PromptAnalysis_v1')
+# process.GlobalTag = GlobalTag(process.GlobalTag, '130X_dataRun3_v2')
+# process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run3_data')
+
+process.MessageLogger.cerr.FwkReport.reportEvery = 500
+process.options = cms.untracked.PSet(
+  wantSummary = cms.untracked.bool(True),
+  allowUnscheduled = cms.untracked.bool(True),
+  SkipEvent = cms.untracked.vstring('ProductNotFound') )
+
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
+
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring('file:test.root')
+)
+
+process.rootuple = cms.EDAnalyzer('miniAODtrigRate',
+                          muons = cms.InputTag("slimmedMuons"),
+                          bits = cms.InputTag("TriggerResults::HLT"),
+                          objects = cms.InputTag("slimmedPatTrigger"),
+                          MuonTrigger = cms.string("HLT_IsoMu24_v"),
+                          isMC = cms.bool(False),
+                          )
+
+process.TFileService = cms.Service("TFileService",
+  fileName = cms.string('Muon1_Run2023C_v3_v1_Data.root'),
+)
+
+#process.p = cms.Path(process.egammaPostRecoSeq+process.rootuple)
+process.p = cms.Path(process.rootuple)
+
