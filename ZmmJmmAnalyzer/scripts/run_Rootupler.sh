158a90f519018e3eb8938783e84a550d612d7328
Nimmitha Karunarathna <nimmithak@gmail.com>
2024-07-17 14:18:22 -0500

    updated rootupler runner

--- ZmmJmmAnalyzer/scripts/run_Rootupler.sh
+++ ZmmJmmAnalyzer/scripts/run_Rootupler.sh
@@ -8,16 +8,14 @@ cmsenv
 echo "Starting the loop to process files..."
 for i in {1..10}; do
     echo "Processing file with index $i..."
-    # sed -i "s/datasets\/ZmmYee\/MiniAOD\/MiniAOD_[0-9]\+\.root/datasets\/ZmmYee\/MiniAOD\/MiniAOD_$i.root/g" miniAODmmmmRootupler.py
-    sed -i "s/datasets\/mc\/run2_zmmJpsimm\/ZmmJpsimm_2018\/MiniAOD\/MiniAOD_[0-9]\+\.root/datasets\/mc\/run2_zmmJpsimm\/ZmmJpsimm_2018\/MiniAOD\/MiniAOD_$i.root/g" miniAODmmmm/python/miniAODmmmmRootupler_1.py
-    # sed -i "s/output\/ZmmMCOut_[0-9]\+\.root/output\/ZmmMCOut_$i.root/g" miniAODmmmmRootupler.py
-    sed -i "s/zmmjmm_mc_2018\/zmmjmm_mc_2018_[0-9]\+\.root/zmmjmm_mc_2018\/zmmjmm_mc_2018_$i.root/g" miniAODmmmm/python/miniAODmmmmRootupler_1.py
-    # cmsRun miniAODmmmmRootupler.py
+    sed -i "s/datasets\/mc\/run2_zmmJpsimm\/ZmmJpsimm_2018\/MiniAOD\/MiniAOD_[0-9]\+\.root/datasets\/mc\/run2_zmmJpsimm\/ZmmJpsimm_2018\/MiniAOD\/MiniAOD_$i.root/g" miniAODmmmm/python/miniAODmuonsRootupler_1.py
+    sed -i "s/zmmjmm_mc_2018\/zmmjmm_mc_2018_[0-9]\+\.root/zmmjmm_mc_2018\/zmmjmm_mc_2018_$i.root/g" miniAODmmmm/python/miniAODmuonsRootupler_1.py
+    cmsRun miniAODmmmm/python/miniAODmuonsRootupler_1.py
 done
 
 # combine the output files
 echo "Combining the output files..."
-# hadd inputFiles/zmmjmm_mc_2018/zmmjmm_mc_2018.root inputFiles/zmmjmm_mc_2018/zmmjmm_mc_*.root
+hadd inputFiles/zmmjmm_mc_2018/zmmjmm_mc_2018.root inputFiles/zmmjmm_mc_2018/zmmjmm_mc_*.root
 
 echo "All done!"
 
