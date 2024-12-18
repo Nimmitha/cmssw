2305d903e04babb62edd6354a3df740c8449a164
Nimmitha Karunarathna <nimmithak@gmail.com>
2024-12-18 04:16:51 -0600

    config files for 2024

--- ZeeJmmAnalyzer/dataSubmit/2024/H0/miniAODmuonsRootupler_2024_E0_H.py
+++ ZeeJmmAnalyzer/dataSubmit/2024/H0/miniAODmuonsRootupler_2024_E0_H.py
@@ -12,37 +12,20 @@ process.load('Configuration.StandardSequences.EndOfProcess_cff')
 
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 from Configuration.AlCa.GlobalTag import GlobalTag
-# process.GlobalTag = GlobalTag(process.GlobalTag, '124X_dataRun3_v15', '') # 2022 B-E
-# process.GlobalTag = GlobalTag(process.GlobalTag, '130X_dataRun3_PromptAnalysis_v1') # 2023
-process.GlobalTag = GlobalTag(process.GlobalTag, '141X_dataRun3_Prompt_v3') # 2023
+process.GlobalTag = GlobalTag(process.GlobalTag, '141X_dataRun3_Prompt_v3')
 
-process.MessageLogger.cerr.FwkReport.reportEvery = 1
+process.MessageLogger.cerr.FwkReport.reportEvery = 500
 process.options = cms.untracked.PSet(
   wantSummary = cms.untracked.bool(True),
   allowUnscheduled = cms.untracked.bool(True),
   # SkipEvent = cms.untracked.vstring('ProductNotFound')
-  )
+)
 
 process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
 
 process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring(       
-#  '/store/data/Run2022B/EGamma/MINIAOD/22Sep2023-v2/70000/01921504-9d3d-4993-a46b-e45dc979a2a4.root'
-#  '/store/data/Run2022B/EGamma/MINIAOD/27Jun2023-v2/2520000/2cbda963-c777-4a15-9f7a-56b1be95099c.root'
-#  '/store/data/Run2023C/EGamma0/MINIAOD/22Sep2023_v1-v1/2530000/0180e051-34b5-47a0-a851-665aa47846fd.root'
-'/store/data/Run2024D/EGamma0/MINIAOD/PromptReco-v1/000/380/306/00000/0c8bf77b-259c-4942-8b8e-041672cde204.root'
- )
+    fileNames = cms.untracked.vstring('file:test.root')
 )
-
-# from PhysicsTools.SelectorUtils.tools.vid_id_tools import *
-# dataFormat = DataFormat.MiniAOD
-# switchOnVIDElectronIdProducer(process, dataFormat)
-# process.load("RecoEgamma.ElectronIdentification.ElectronMVAValueMapProducer_cfi")
-
-# my_id_modules = [
-#     'RecoEgamma.ElectronIdentification.Identification.cutBasedElectronID_RunIIIWinter22_V1_cff'
-# ]
-
 
 process.rootuple = cms.EDAnalyzer('miniAODeemm',
                           dimuons = cms.InputTag("slimmedMuons"),
@@ -56,9 +39,7 @@ process.rootuple = cms.EDAnalyzer('miniAODeemm',
                           )
 
 process.TFileService = cms.Service("TFileService",
-  fileName = cms.string('Testout_2024D_zeeymm.root'),
+  fileName = cms.string('EGamma0_Run2024H_PromptReco_v1_Data.root'),
 )
 
-# process.p = cms.Path(process.electronMVAValueMapProducer + process.rootuple)
 process.p = cms.Path(process.rootuple)
-
