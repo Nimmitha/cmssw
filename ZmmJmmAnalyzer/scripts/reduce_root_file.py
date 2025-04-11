071037178c9ac4b1ef4904cb42e4fb43dabf84dc
Nimmitha Karunarathna <nimmithak@gmail.com>
2025-04-10 21:05:09 -0500

    optimze the use of varible names

--- ZmmJmmAnalyzer/scripts/reduce_root_file.py
+++ ZmmJmmAnalyzer/scripts/reduce_root_file.py
@@ -2,28 +2,60 @@ import uproot
 import awkward as ak
 
 base_path = '/uscms/home/wkarunar/nobackup/datasets/data/run3/parkingDoubleMuonLowMass/2023/allTrig/'
+# base_path = '/uscms/home/wkarunar/nobackup/datasets/data/run3/Muon/jmm/'
 
-# List of input files
 input_files = [
+    # 'crab_TTree_136TeV_Muon0_mm_2023D_v1.root',
+    # 'crab_TTree_136TeV_Muon1_mm_2023D_v1.root',
     'crab_TTree_136TeV_PDMLM0_mm_2023D_v1.root',
     'crab_TTree_136TeV_PDMLM4_mm_2023D_v1.root',
     'crab_TTree_136TeV_PDMLM5_mm_2023D_v1.root',
-    'crab_TTree_136TeV_PDMLM7_mm_2023D_v1.root'
-]
-
-# Variables to keep
-branches = [
-    "Event", "Run", "LumiBlock",
-    "B_J1_mass", "B_J1_pt",
-    "B_J1_VtxPt", "B_J1_VtxProb", "B_J1_VtxMass",
-    "B_Mu1_pt", "B_Mu2_pt", "B_M1_pt", "B_M2_pt",
-    "B_Mu1_soft", "B_Mu2_soft", "B_Mu1_loose", "B_Mu2_loose", 
-    "savedtriggerNames", "savedtriggerBits"
+    'crab_TTree_136TeV_PDMLM7_mm_2023D_v1.root',
 ]
 
 # Trigger to check
 trigger_tag = "HLT_Mu0_L1DoubleMu_v5"
+# trigger_tag = "HLT_IsoMu24_v17"
 batch_size = 10000  # Process 10k events at a time
+
+# List of variables you want to save
+variables_to_save = [
+    "Event", "Run", "LumiBlock",
+    "B_J1_mass", "B_J1_pt", "B_J1_VtxProb", "B_J1_VtxMass",
+    "B_Mu1_pt", "B_Mu2_pt", "B_M1_pt", "B_M2_pt",
+    "B_Mu1_eta", "B_Mu2_eta", "B_M1_eta", "B_M2_eta",
+    "B_Mu1_soft", "B_Mu2_soft", "B_Mu1_loose", "B_Mu2_loose",
+    "B_Mu1_tight", "B_Mu2_tight"
+]
+
+# Data types (you can edit this easily later too)
+output_branches_types = {
+    "Event": "uint64",
+    "Run": "uint32",
+    "LumiBlock": "uint32",
+    "B_J1_mass": "float32",
+    "B_J1_pt": "float32",
+    "B_J1_VtxProb": "float32",
+    "B_J1_VtxMass": "float32",
+    "B_Mu1_pt": "float32",
+    "B_Mu2_pt": "float32",
+    "B_M1_pt": "float32",
+    "B_M2_pt": "float32",
+    "B_Mu1_eta": "float32",
+    "B_Mu2_eta": "float32",
+    "B_M1_eta": "float32",
+    "B_M2_eta": "float32",
+    "B_Mu1_soft": "bool",
+    "B_Mu2_soft": "bool",
+    "B_Mu1_loose": "bool",
+    "B_Mu2_loose": "bool",
+    "B_Mu1_tight": "bool",
+    "B_Mu2_tight": "bool",
+    "TriggerFired": "bool",
+}
+
+# Branches needed from input file
+branches_to_read = variables_to_save + ["savedtriggerNames", "savedtriggerBits"]
 
 def process_file(base_path, file_path, trigger_tag):
     """
@@ -34,62 +66,31 @@ def process_file(base_path, file_path, trigger_tag):
     tree_in = file_in["ntuple"]
     nEntries = tree_in.num_entries
 
-    # Create a new ROOT file for this input
+    # Create output file
     output_file = f"{file_path[:-5]}_{trigger_tag}.root"
 
     with uproot.recreate(output_file) as fout:
-        fout.mktree("tree", {
-            "Event": "uint64",
-            "Run": "uint32",
-            "LumiBlock": "uint32",
-            "B_J1_mass": "float32",
-            "B_J1_pt": "float32",
-            "B_J1_VtxProb": "float32",
-            "B_J1_VtxMass": "float32",
-            "B_Mu1_pt": "float32",
-            "B_Mu2_pt": "float32",
-            "B_M1_pt": "float32",
-            "B_M2_pt": "float32",
-            "B_Mu1_soft": "bool",
-            "B_Mu2_soft": "bool",
-            "B_Mu1_loose": "bool",
-            "B_Mu2_loose": "bool",
-        })
+        fout.mktree("tree", output_branches_types)
 
-        for i, arrays in enumerate(tree_in.iterate(branches, step_size=batch_size)):
+        for i, arrays in enumerate(tree_in.iterate(branches_to_read, step_size=batch_size)):
             print(f"Processing {i*batch_size}/{nEntries}")
 
-            # Find events where the trigger fired
+            # Find if trigger fired (bool array)
             trigger_fired = ak.any(
                 (arrays["savedtriggerNames"] == trigger_tag) & (arrays["savedtriggerBits"]),
                 axis=1
             )
 
-            selected = arrays[trigger_fired]
+            output = {}
+            for var in variables_to_save:
+                output[var] = arrays[var]
+            # Add the trigger status (always True because we select on it, but you could generalize)
+            output["TriggerFired"] = trigger_fired
 
-            if len(selected) > 0:
-                output = {
-                    "Event": selected["Event"],
-                    "Run": selected["Run"],
-                    "LumiBlock": selected["LumiBlock"],
-                    "B_J1_mass": selected["B_J1_mass"],
-                    "B_J1_pt": selected["B_J1_pt"],
-                    "B_J1_VtxProb": selected["B_J1_VtxProb"],
-                    "B_J1_VtxMass": selected["B_J1_VtxMass"],
-                    "B_Mu1_pt": selected["B_Mu1_pt"],
-                    "B_Mu2_pt": selected["B_Mu2_pt"],
-                    "B_M1_pt": selected["B_M1_pt"],
-                    "B_M2_pt": selected["B_M2_pt"],
-                    "B_Mu1_soft": selected["B_Mu1_soft"],
-                    "B_Mu2_soft": selected["B_Mu2_soft"],
-                    "B_Mu1_loose": selected["B_Mu1_loose"],
-                    "B_Mu2_loose": selected["B_Mu2_loose"],
-                }
-                fout["tree"].extend(output)
+            fout["tree"].extend(output)
 
-    file_in.close()  # Close the input file
+    file_in.close()
     print(f"Finished writing {output_file}")
-    
 
 for file_idx, file_path in enumerate(input_files):
     print(f"Opening file {file_idx+1}/{len(input_files)}: {file_path}")
