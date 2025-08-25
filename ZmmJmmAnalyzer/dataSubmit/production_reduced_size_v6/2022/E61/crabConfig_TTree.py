26ecda550c34e9f81b884b0ab397b8e1037a0ea1
Nimmitha Karunarathna <nimmithak@gmail.com>
2025-08-25 00:52:54 -0500

    2022 config | lumi

---
+++ ZmmJmmAnalyzer/dataSubmit/production_reduced_size_v6/2022/E61/crabConfig_TTree.py
@@ -0,0 +1,30 @@
+from CRABClient.UserUtilities import config
+config = config()
+
+# user specific generic parameters
+config.General.requestName = 'PDMLM6_mm_2022E1_v6'   # Used as the task/Project directory name
+config.General.transferOutputs = True                               # Transfer output files to the storage site
+config.General.transferLogs = False
+
+# job type and related configurables
+config.JobType.pluginName = 'Analysis'                              # Specify: analysis or MC generation
+config.JobType.psetName = 'miniAODmuonsRootupler.py'           # parameter-set config file
+config.JobType.allowUndistributedCMSSW = True                       # Allow CMSSW release possibly not available at sites
+config.JobType.outputFiles = ['PDMLM6_Run2022E1_v2_Data.root']   # List of output files that needs to be collected
+config.JobType.maxJobRuntimeMin = 60                                  # Maximum job runtime in minutes
+# config.JobType.maxMemoryMB = 1000
+
+# data to be analyzed
+config.Data.inputDBS = 'global'
+config.Data.inputDataset = '/ParkingDoubleMuonLowMass6/Run2022E-10Dec2022-v2/MINIAOD'               # Name of the dataset
+#config.Data.outputPrimaryDataset = 'DoubleJPsiToMuMu_RAWSIM_SPS_LO_may2016_largetest_FNAL' # Used when running private input files or MC generation
+config.Data.lumiMask = 'Cert_Collisions2022_355100_362760_Golden.json' # Lumi-section filter
+config.Data.splitting = 'LumiBased'                                                         # Split the task based on 
+config.Data.unitsPerJob = 50                                                                # Number of splitted units per job
+#NJOBS = 500  # This is not a configuration parameter, but an auxiliary variable that we use in the next line.
+config.Data.totalUnits = -1                                                                 # Number of untis to analyze 
+config.Data.outLFNDirBase = '/store/user/nkarunar/atlas_compare/'
+config.Data.publication = False                                                             # Whether to publish the EDM output files in DBS
+
+# Grid site parameters
+config.Site.storageSite = 'T3_US_FNALLPC'         # Place to copy the output files
