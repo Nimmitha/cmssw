28dd9e72f934deda9c4d2e1dadf849d74413d80e
Nimmitha Karunarathna <nimmithak@gmail.com>
2025-09-01 20:15:17 -0500

    updated global tag for 2025

--- ZmmJmmAnalyzer/miniAODmmmm/python/runGenStudyAnalyzer.py
+++ ZmmJmmAnalyzer/miniAODmmmm/python/runGenStudyAnalyzer.py
@@ -12,7 +12,8 @@ process.load('Configuration.StandardSequences.EndOfProcess_cff')
 
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 from Configuration.AlCa.GlobalTag import GlobalTag
-process.GlobalTag = GlobalTag(process.GlobalTag, '133X_mcRun3_2024_realistic_v8') # JPsiToMuMu MC
+# process.GlobalTag = GlobalTag(process.GlobalTag, '133X_mcRun3_2024_realistic_v8') # JPsiToMuMu MC
+process.GlobalTag = GlobalTag(process.GlobalTag, '142X_mcRun3_2025_realistic_v7') # JPsiToMuMu MC25
 
 
 # process.MessageLogger.cerr.FwkReport.reportEvery = 500
@@ -43,7 +44,8 @@ process.options = cms.untracked.PSet(
 process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(       
-        'file:miniAODs/1d0b3161-17d1-450d-bca4-1898c5268117_Run3WinterMC.root'
+        # 'file:miniAODs/1d0b3161-17d1-450d-bca4-1898c5268117_Run3WinterMC.root'
+        '/store/mc/Run3Winter25MiniAOD/JPsiToMuMu_PT-0to100_pythia8-gun/MINIAODSIM/FlatPU0to120_MiniAODv6_150X_mcRun3_2025_realistic_v6-v2/120000/6b784ff3-0f02-478c-9ba6-e9e8f4c8ca7a.root'
  )
 )
 
@@ -59,7 +61,7 @@ process.rootuple = cms.EDAnalyzer('GenStudyAnalyzer',
                           )
 
 process.TFileService = cms.Service("TFileService",
-  fileName = cms.string('preselection/testout_2024WinterMC.root'),
+  fileName = cms.string('preselection/testout_2025WinterMC.root'),
 )
 
 process.p = cms.Path(process.rootuple)
