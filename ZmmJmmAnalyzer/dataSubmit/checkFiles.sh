35d0e837688f04e6dd11b424378cae39be47a4f2
Nimmitha Karunarathna <nimmithak@gmail.com>
2026-05-26 00:02:53 -0500

    added config files

---
+++ ZmmJmmAnalyzer/dataSubmit/checkFiles.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Check if the number of arguments is correct
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <year> <line_number>"
+    exit 1
+fi
+
+year=$1
+line_number=$2
+filename_pattern="crabConfig_TTree.py"
+# filename_pattern="miniAODmmmmRootupler_*.py"
+
+echo "Searching for line $line_number in files matching pattern '$filename_pattern' for year $year..."
+
+# Loop over files matching the pattern
+for file in "$year"/*/$filename_pattern; do
+    # Check if the file exists
+    if [ -f "$file" ]; then
+        echo "$file"
+        # Print the specified line number from the file
+        sed -n "${line_number}p" "$file"
+        echo
+    fi
+done
